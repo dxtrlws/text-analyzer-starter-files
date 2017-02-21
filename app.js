4f78bb7f9198491d34394a5289b2a50e6bbe5b30
Dexter Lewis <dexterlewis@gmail.com>
2017-02-21 12:45:07 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // your code here!
+function totalWords (words) {
+    return words.split(' ').length;
+}
+
+function averageLength (words) {
+
+}
+
+function renderResults(words) {
+
+}
+
+
+function analyzeForm () {
+ $('button').click(function(){
+    var words = $('textarea').val();
+    $('.text-report').removeClass('hidden');
+    superCounter(words);
+ });
+
+}
+
+
+$(function() {
+analyzeForm();
+
+});
