4a2e4ad40e9d503e15f65cd6ef494e4fa2704c57
Dexter Lewis <dexterlewis@gmail.com>
2017-02-21 13:41:44 -0500

    finished app.js but code isn't working

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 // your code here!
+// calculate word length
 function wordCount(text) {
     return text.split(" ").length;
 
 }
 
+//find average length of words
 function averageLength (text) {
     var totalLength = text.join(" ").length;
     return totalLength / text.length;
 }
 
+//find unique words
 function uniqueWords(text) {
-    
+    var unique = [];
+    for (var i=0; i< text.length; i++) {
+        if (unique.indexOf(text[i]) === -1) {
+            unique.push(text[i]);
+        }
+    }
+    return unique.length;
 }
 
-
+//render results to the DOM
 function renderResults(text) {
     var totalWordLength = wordCount(text);
-    var distictWords = uniqueWords(text);
-    var avgLenth = averageLength(text);
+    var distinctWords = uniqueWords(text);
+    var averageWordLength = averageLength(text);
 
     $('.js-wordCount').text(totalWordLength);
-    $('.js-uniqueWords').text(distictWords);
-    $('.js-averageLength').text(avgLenth);
+    $('.js-uniqueWords').text(distinctWords);
+    $('.js-averageLength').text(averageWordLength);
 }
 
 
